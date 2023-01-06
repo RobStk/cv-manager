bdec5fe0e5515c82ffb72e160ac64a05815ad949
robster <99953834+RobStk@users.noreply.github.com>
2023-01-06 09:44:16 +0100

    data-service

---
+++ src/modules/data-service.js
@@ -0,0 +1,106 @@
+class DataService {
+
+	/* ---------------------------------------------------- */
+	/* Interface 											*/
+	/* ---------------------------------------------------- */
+	get getData() { return this.#getData; }
+	get addItem() { return this.#addItem; }
+	get updateItem() { return this.#updateItem; }
+
+	/* ---------------------------------------------------- */
+	/* Constructor 											*/
+	/* ---------------------------------------------------- */
+	/**
+	 * @param {string} url 
+	 */
+	constructor(url) {
+		this.#url = url;
+	}
+
+	/* ---------------------------------------------------- */
+	/* Private properties 									*/
+	/* ---------------------------------------------------- */
+	#url;
+
+	/* ---------------------------------------------------- */
+	/* Methods 												*/
+	/* ---------------------------------------------------- */
+
+	/**
+	 * Returns a promise resolved to a data array or null.
+	 * @returns {Promise<Array|null>}
+	 */
+	async #getData() {
+		try {
+			const response = await fetch(this.#url);
+			if (!response.ok) throw new Error(response.status);
+			const data = await response.json();
+			return data;
+		}
+		catch (error) {
+			console.error("Http error:", error.status || error);
+			return null;
+		}
+	}
+
+	// ----------------------------------------------------
+
+	/**
+	 * Adds data and return true if resolved or false if rejected.
+	 * @param {Object} itemToAdd 
+	 * @returns {Promise<boolean>}
+	 */
+	async #addItem(itemToAdd) {
+		const dataString = JSON.stringify(itemToAdd);
+		try {
+			const response = await fetch(this.#url, {
+				method: "post",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: dataString
+			});
+			if (!response.ok) throw new Error(response.status);
+			const jsonResult = await response.json();
+			const id = jsonResult.id;
+			return id;
+		}
+		catch (error) {
+			console.error("Http error:", error.status || error);
+			return null;
+		}
+	}
+
+	// ----------------------------------------------------
+
+	/**
+	 * Updates data and return true if resolved or false if rejected.
+	 * @param {Object} itemToUpdate 
+	 * @returns {Promise<boolean>}
+	 */
+	async #updateItem(itemToUpdate) {
+		const id = itemToUpdate.id;
+		const tempId = itemToUpdate.tempId;
+		if (id && tempId) delete itemToUpdate.tempId;
+		const dataString = JSON.stringify(itemToUpdate);
+		try {
+			const response = await fetch(this.#url + "/" + id, {
+				method: "put",
+				headers: {
+					"Content-Type": "application/json"
+				},
+				body: dataString
+			});
+			if (!response.ok) throw new Error(response.status);
+			return true;
+		}
+		catch (error) {
+			console.error("Http error:", error.status || error);
+			return false;
+		}
+	}
+
+	// ----------------------------------------------------
+}
+
+export default DataService;
