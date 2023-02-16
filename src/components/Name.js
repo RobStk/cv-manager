a18d8ec0b5740c6c1daf0d041ebdecfe664b09a9
robster <99953834+RobStk@users.noreply.github.com>
2023-02-16 19:13:44 +0100

    Name Component update

--- src/components/Name.js
+++ src/components/Name.js
@@ -1,37 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { ThemeContext, ThemeProvider } from "styled-components";
 import propTypes from "prop-types";
 import NameStyled from "./NameStyled";
 import EditableDataComponent from "./EditableDataComponent";
 import Header from "./Header";
+import createInputBatch from "./factories/inputBatchFactory";
 
 export default function Name(props) {
 	const theme = props.theme || useContext(ThemeContext);
 	const dao = props.dao;
 	const data = dao.getData() || {};
-	const nameInput = {
-		inputType: "text",
-		id: "nameValueInput",
-		name: "Name",
-		value: data.value,
-		label: "Name"
-	};
+	const nameInputRef = useRef();
+	const inputBatch = createInputBatch({ type: "value-short", ref: nameInputRef, value: data.value });
 
 	return (
 		<ThemeProvider theme={theme}>
 			<NameStyled className={props.className}>
-				<EditableDataComponent inputsData={[nameInput]} onUpdate={handleUpdate}>
+				<EditableDataComponent inputBatches={inputBatch} onUpdate={handleUpdate}>
 					<Header>{data.value}</Header>
 				</EditableDataComponent>
 			</NameStyled>
 		</ThemeProvider>
 	);
 
-	function handleUpdate(inputsData) {
+	function handleUpdate() {
 		const newData = { ...data };
-		inputsData.forEach(input => {
-			if (input.id == nameInput.id) newData.value = input.value;
-		});
+		newData.value = nameInputRef.current.value;
 		dao.setData(newData);
 	}
 }
