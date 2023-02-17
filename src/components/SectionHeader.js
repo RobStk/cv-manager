9b48f34ccb27785d4c5a03cb19b1a9f028993e8c
robster <99953834+RobStk@users.noreply.github.com>
2023-02-17 09:24:12 +0100

    SectionHeader update

--- src/components/SectionHeader.js
+++ src/components/SectionHeader.js
@@ -1,45 +1,40 @@
-import React from "react";
+import React, { useRef } from "react";
 import SectionHeaderStyled from "./SectionHeaderStyled";
 import Header from "./Header";
 import PropTypes from "prop-types";
 import EditableDataComponent from "./EditableDataComponent";
 import { ThemeProvider } from "styled-components";
 import simpleTheme from "../main-styles/themes/simple-theme";
+import createInputBatch from "./factories/inputBatchFactory";
 
 export default function SectionHeader(props) {
 	const theme = props.theme || simpleTheme;
-	const input = {
-		inputType: "text",
-		id: "headerId",
-		name: "Header",
+
+	const inputRef = useRef();
+	const inputBatch = createInputBatch({
+		ref: inputRef,
+		type: "title",
 		value: props.data,
-		label: props.label || "Header"
-	};
+	});
 
 	return (
 		<ThemeProvider theme={theme}>
 			<SectionHeaderStyled className={props.className || "header"}>
-				<EditableDataComponent inputsData={[input]} onUpdate={handleUpdate}>
+				<EditableDataComponent inputBatches={inputBatch} onUpdate={handleUpdate}>
 					<Header>{props.data}</Header>
 				</EditableDataComponent>
 			</SectionHeaderStyled>
 		</ThemeProvider>
 	);
 
-	function handleUpdate(inputsData) {
-		let newValue = "";
-		inputsData.forEach(input => {
-			if (input.id == input.id) (
-				newValue = input.value
-			);
-		});
+	function handleUpdate() {
+		const newValue = inputRef.current.value;
 		props.onUpdate(newValue);
 	}
 }
 
 SectionHeader.propTypes = {
 	data: PropTypes.string,
-	label: PropTypes.string,
 	theme: PropTypes.object,
 	className: PropTypes.string,
 	onUpdate: PropTypes.func
