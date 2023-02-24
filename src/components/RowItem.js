d801b2c760488fc95545460e78e8c6f39c9d0cd8
robster <99953834+RobStk@users.noreply.github.com>
2023-02-24 07:55:12 +0100

    RowItem without title

--- src/components/RowItem.js
+++ src/components/RowItem.js
@@ -2,10 +2,12 @@ import React from "react";
 import propTypes from "prop-types";
 import RowItemStyled from "./RowItemStyled";
 
-export default function RowItem({ title, value }) {
+export default function RowItem(props) {
+	const title = props.title ? `${props.title}:` : "";
+	const value = props.value;
 	return (
 		<RowItemStyled>
-			<span className="row-title">{title + ":"}</span>
+			<span className="row-title">{title}</span>
 			<span >{" "}</span>
 			<span className="row-value">{value}</span>
 		</RowItemStyled>
